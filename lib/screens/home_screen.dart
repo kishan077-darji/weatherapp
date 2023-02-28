67e0b83782948f0a1906dffac26bf7bfa69bb5d8
Kishan <darjikishan2002@gmail.com>
2023-02-28 10:54:13 +0530

    containers

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,71 +13,157 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    print("This is init state");
   }
 
   @override
   void setState(VoidCallback fn) {
     super.setState(fn);
-    print("set state called");
   }
 
   @override
   void dispose() {
     super.dispose();
-    print("Widget distroyed");
   }
 
   @override
   Widget build(BuildContext context) {
     Map? info = ModalRoute.of(context)!.settings.arguments as Map?;
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(0),
+        child: AppBar(
+          backgroundColor: Colors.blue[400],
+        ),
+      ),
       body: SafeArea(
         child: Container(
+          height: 900,
           decoration: BoxDecoration(
               // color: Colors.black54,
               gradient: LinearGradient(colors: [
             Colors.blue[300]!,
             Colors.blue[800]!,
           ])),
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 20,
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(0),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 20,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Search any city here",
+                              hintStyle:
+                                  TextStyle(fontWeight: FontWeight.w700)),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: const Icon(
+                          Icons.search,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                    ],
+                  ),
                 ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(40),
-                ),
-                child: Row(
+                Row(
                   children: [
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    const Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Search any city here",
-                            hintStyle: TextStyle(fontWeight: FontWeight.w700)),
+                    Expanded(
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        padding: const EdgeInsets.all(30),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        child: const Text("Text"),
                       ),
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: const Icon(
-                        Icons.search,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 10,
                     ),
                   ],
                 ),
-              ),
-            ],
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 280,
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 20),
+                        padding: const EdgeInsets.all(50),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        child: const Text("Text"),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 150,
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        padding: const EdgeInsets.all(30),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        child: const Text("Text"),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 150,
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        padding: const EdgeInsets.all(30),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        child: const Text("Text"),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.all(40),
+                  child: Column(children: const [
+                    Text(
+                      "Made By Kishan",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      "Data Provided By Openweather.org",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ]),
+                ),
+              ],
+            ),
           ),
         ),
       ),
